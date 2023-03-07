e35b2bb357ce7225dc0251e2c13e20ae4395d203
David Bischof <bischof.david.db@gmail.com>
2023-03-07 09:08:43 +0100

    functions simplyfied

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counterguest, setCounterguest] = useState(0)
+  const [winpercentguest, setWinpercentguest] = useState(0)
+  const [counterheim, setCounterheim] = useState(0)
+  const [winpercentheim, setWinpercentheim] = useState(0)
+  const [turn, setTurn] = useState (1)
+
+
+
+
+  function points(counter, setCounter, setWin, points, chance){
+    var randomNumber = Math.floor((Math.random() * 100) + 1);
+    if(randomNumber<=chance){
+      let tmp = counter + points
+      setCounter(tmp)
+      setWin(tmp*100)
+    }
+if(turn == 1){
+  setTurn(0)
+}
+else{
+  setTurn(1)
+}
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <br/>
+<div>Heim</div>
+     <input type="text" placeholder="Heim Name"/>
+      <br/>
+      <div>Punkte von Heim: {counterheim}</div>
+      <input type="button" value="2 Punkt Wurf" onClick={()=>{points(counterheim, setCounterheim, setWinpercentheim, 2, 80); }} disabled={turn === 0}/>
+      <input type="button" value="3 Punkte Wurf" onClick={()=>{points(counterheim, setCounterheim, setWinpercentheim, 3, 50)}} disabled={turn === 0}/>
+      <div>In % bis zum Sieg: {winpercentheim}%</div>
+     <br/>
+     <div>Gast</div>
+     <input type="text" placeholder="Gast Name"/>
+      <br/>
+      <div>Punkte von Gast: {counterguest}</div>
+      <input type="button" value="2 Punkt Wurf" onClick={()=>points(counterguest, setCounterguest, setWinpercentguest, 2, 80)} disabled={turn === 1}/>
+      <input type="button" value="3 Punkte Wurf" onClick={()=>points(counterguest, setCounterguest, setWinpercentguest, 3, 50)} disabled={turn === 1}/>
+      <div>In % bis zum Sieg: {winpercentguest}%</div>
+
+
+
     </div>
   );
 }
